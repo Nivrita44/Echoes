0a5d1380af62255c50bd6b1af71d1197787e1499
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2024-05-30 11:54:40 +0600

    homepage

---
+++ client/src/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div className="px-4 lg:px-24 bg-teal-100 flex items-center">
+      <div className="flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40">
+        <div>left</div>
+        <div>right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
