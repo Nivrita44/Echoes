a5ff536303e8043b6096c5ccebe5bf5ab34161ad
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2024-05-30 17:51:04 +0600

    Table Creation

--- server/index.js
+++ server/index.js
@@ -16,15 +16,39 @@ const con = mysql.createConnection({
 
 con.connect(function (err) {
   if (err) {
-    console.log("not Connected");
+    console.error("Error connecting to MySQL database:", err);
+    process.exit(1); // Exit the process with an error code
+  } else {
+    console.log("Connected to MySQL database!");
+
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS books (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        book_title VARCHAR(255) NOT NULL,
+        author VARCHAR(255) NOT NULL,
+        published_date DATE,
+        description VARCHAR(1000),
+        image_url VARCHAR(255),
+        category VARCHAR(50)
+      )
+    `;
+
+    con.query(createTableQuery, function (err, result) {
+      if (err) {
+        console.error("Table creation failed:", err);
+        process.exit(1); // Exit the process with an error code
+      } else {
+        console.log("Table created or already exists");
+
+        // Start the server only after table creation
+        app.listen(port, () => {
+          console.log(`Server is connected to port ${port}`);
+        });
+      }
+    });
   }
-  console.log("Connected");
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.listen(port, () => {
-  console.log(`Server is connected to port ${port}`);
+  res.send("Hello World!!!");
 });
