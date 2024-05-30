dfbaa4541592d3b636de2a311bf9c9b79f8ce487
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2024-05-30 13:28:55 +0600

    database_insertion

--- server/index.js
+++ server/index.js
@@ -1,15 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 2000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const port = 2000;
+const cors = require("cors");
+const mysql = require("mysql");
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send()
-})
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "echoes_book",
+});
+
+con.connect(function (err) {
+  if (err) {
+    console.log("not Connected");
+  }
+  console.log("Connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
-    console.log(`Server is connected to port ${port}`)
-})
+  console.log(`Server is connected to port ${port}`);
+});
