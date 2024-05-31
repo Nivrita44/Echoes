29558099851804e5fc386667b170c47957e441a8
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2024-05-31 23:22:47 +0600

    loginRegisterPageWithoutDatabase

--- client/src/routers/router.jsx
+++ client/src/routers/router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../components/About";
 import Blog from "../components/Blog";
+import Dashboard from "../components/Dashboard/Dashboard";
+import Register from "../components/Register/Register";
+import Login from "../components/login/Login";
 import Home from "../home/Home";
 import Shop from "../shop/Shop";
 const router = createBrowserRouter([
@@ -24,6 +27,18 @@ const router = createBrowserRouter([
       {
         path: "/blog",
         element: <Blog />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
       },
     ],
   },
