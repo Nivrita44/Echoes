a7966067b5414fbaec1476f88dba1fb2b14f3d85
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2024-06-02 22:52:23 +0600

    Commit

--- client/src/routers/router.jsx
+++ client/src/routers/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../components/About";
 import Blog from "../components/Blog";
+// import Dashboard from "../components/Dashboard/Dashboard";
 import Register from "../components/Register/Register";
 import Login from "../components/login/Login";
 import Home from "../home/Home";
@@ -35,6 +36,10 @@ const router = createBrowserRouter([
         path: "/register",
         element: <Register />,
       },
+      // {
+      //   path: "/dashboard",
+      //   element: <Dashboard />,
+      // },
     ],
   },
 ]);
