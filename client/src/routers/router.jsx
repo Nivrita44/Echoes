c6bbfd361e6b8c5ed85dff83407b53dc0de0e01b
Nivrita44 <107574615+Nivrita44@users.noreply.github.com>
2024-06-02 16:35:44 +0600

    Update router.jsx

--- client/src/routers/router.jsx
+++ client/src/routers/router.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../components/About";
 import Blog from "../components/Blog";
-import Dashboard from "../components/Dashboard/Dashboard";
 import Register from "../components/Register/Register";
 import Login from "../components/login/Login";
 import Home from "../home/Home";
@@ -35,10 +34,6 @@ const router = createBrowserRouter([
       {
         path: "/register",
         element: <Register />,
-      },
-      {
-        path: "/dashboard",
-        element: <Dashboard />,
       },
     ],
   },
