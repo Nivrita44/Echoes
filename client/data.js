13ad751bd99d3a139eafaec66414184c38f6f8e4
DURBAroy111 <120655973+DURBAroy111@users.noreply.github.com>
2024-06-04 01:59:51 +0600

    Top Category

---
+++ client/data.js
@@ -0,0 +1,213 @@
+import sweicon from "./src/components/Asset/sweicon.png";
+import cseicon from "./src/components/Asset/cseicon.png";
+import staticon from "./src/components/Asset/staticon.png";
+import chemicon from "./src/components/Asset/chemicon.png";
+import biochemicon from "./src/components/Asset/biochemicon.png";
+import Chemicalicon from "./src/components/Asset/Chemicalicon.png";
+import physicsicon from "./src/components/Asset/physics.png";
+import meeicon from "./src/components/Asset/meeicon.png";
+import pmeicon from "./src/components/Asset/pmeicon.png";
+
+import swe from "./src/components/Asset/swe.jpg";
+import cse from "./src/components/Asset/Cse.jpg";
+import stat from "./src/components/Asset/stat.jpg";
+import chem from "./src/components/Asset/che.jpg";
+import biochem from "./src/components/Asset/Biochemistry.jpg";
+import Chemical from "./src/components/Asset/Chemical.jpg";
+import physics from "./src/components/Asset/phy.jpg";
+import mee from "./src/components/Asset/mee.jpg";
+import pme from "./src/components/Asset/pme.jpg";
+
+export const categories = [
+  {
+    img: swe,
+    label: "Software Engineering",
+    icon: sweicon,
+    description: "This property is has windmills!",
+  },
+  {
+    img: cse,
+    label: "Computer Science Engineering",
+    icon: cseicon,
+    description: "This property is has windmills!",
+  },
+  {
+    img: stat,
+    label: "Statistics",
+    icon: staticon,
+    description: "This property is modern!",
+  },
+  {
+    img: physics,
+    label: "Physics",
+    icon: physicsicon,
+    description: "This property is in the countryside!",
+  },
+  {
+    img: pme,
+    label: "PME",
+    icon: pmeicon,
+    description: "This is property has a beautiful pool!",
+  },
+  {
+    img: chem,
+    label: "Chemistry",
+    icon: chemicon,
+    description: "This property is on an island!",
+  },
+  {
+    img: Chemical,
+    label: "Chemical Engineering",
+    icon: Chemicalicon,
+    description: "This property is near a lake!",
+  },
+  {
+    img: mee,
+    label: "Mechanical",
+    icon: meeicon,
+    description: "This property has skiing activies!",
+  },
+  {
+    img: pme,
+    label: "Polymer",
+    icon: pmeicon,
+    description: "This property is an ancient castle!",
+  },
+  {
+    img: biochem,
+    label: "BioChemistry",
+    icon: biochemicon,
+    description: "This property is in a spooky cave!",
+  },
+];
+
+// export const types = [
+//   {
+//     name: "An entire place",
+//     description: "Guests have the whole place to themselves",
+//     icon: <FaHouseUser />,
+//   },
+//   {
+//     name: "Room(s)",
+//     description:
+//       "Guests have their own room in a house, plus access to shared places",
+//     icon: <BsFillDoorOpenFill />,
+//   },
+//   {
+//     name: "A Shared Room",
+//     description:
+//       "Guests sleep in a room or common area that maybe shared with you or others",
+//     icon: <FaPeopleRoof />,
+//   },
+// ];
+
+// export const facilities = [
+//   {
+//     name: "Bath tub",
+//     icon: <PiBathtubFill />,
+//   },
+//   {
+//     name: "Personal care products",
+//     icon: <FaPumpSoap />,
+//   },
+//   {
+//     name: "Outdoor shower",
+//     icon: <FaShower />,
+//   },
+//   {
+//     name: "Washer",
+//     icon: <BiSolidWasher />,
+//   },
+//   {
+//     name: "Dryer",
+//     icon: <BiSolidDryer />,
+//   },
+//   {
+//     name: "Hangers",
+//     icon: <PiCoatHangerFill />,
+//   },
+//   {
+//     name: "Iron",
+//     icon: <TbIroning3 />,
+//   },
+//   {
+//     name: "TV",
+//     icon: <PiTelevisionFill />,
+//   },
+//   {
+//     name: "Dedicated workspace",
+//     icon: <BsPersonWorkspace />,
+//   },
+//   {
+//     name: "Air Conditioning",
+//     icon: <BsSnow />,
+//   },
+//   {
+//     name: "Heating",
+//     icon: <GiHeatHaze />,
+//   },
+//   {
+//     name: "Security cameras",
+//     icon: <GiCctvCamera />,
+//   },
+//   {
+//     name: "Fire extinguisher",
+//     icon: <FaFireExtinguisher />,
+//   },
+//   {
+//     name: "First Aid",
+//     icon: <BiSolidFirstAid />,
+//   },
+//   {
+//     name: "Wifi",
+//     icon: <BiWifi />,
+//   },
+//   {
+//     name: "Cooking set",
+//     icon: <FaKitchenSet />,
+//   },
+//   {
+//     name: "Refrigerator",
+//     icon: <BiSolidFridge />,
+//   },
+//   {
+//     name: "Microwave",
+//     icon: <MdMicrowave />,
+//   },
+//   {
+//     name: "Stove",
+//     icon: <GiToaster />,
+//   },
+//   {
+//     name: "Barbecue grill",
+//     icon: <GiBarbecue />,
+//   },
+//   {
+//     name: "Outdoor dining area",
+//     icon: <FaUmbrellaBeach />,
+//   },
+//   {
+//     name: "Private patio or Balcony",
+//     icon: <MdBalcony />,
+//   },
+//   {
+//     name: "Camp fire",
+//     icon: <GiCampfire />,
+//   },
+//   {
+//     name: "Garden",
+//     icon: <MdYard />,
+//   },
+//   {
+//     name: "Free parking",
+//     icon: <AiFillCar />,
+//   },
+//   {
+//     name: "Self check-in",
+//     icon: <FaKey />,
+//   },
+//   {
+//     name: " Pet allowed",
+//     icon: <MdPets />,
+//   },
+// ];
